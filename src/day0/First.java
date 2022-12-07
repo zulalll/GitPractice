65bc2aa4a6beb837bedfaca566075ce4ac071f04
zulalll <zkutluer10@gmail.com>
2022-12-07 15:02:16 +0300

    first commit

---
+++ src/day0/First.java
@@ -0,0 +1,7 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("First");
+    }
+}
---
+++ src/day0/JamesBond.java
@@ -0,0 +1,7 @@
+package day0;
+
+public class JamesBond {
+    public static void main(String[] args) {
+        System.out.println("James Bond.");
+    }
+}
